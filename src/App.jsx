4874f0a80056b8b994ab0b1c93f8237bf5c3456e
asepsyaepull <asep.syaepul95@gmail.com>
2024-12-07 16:01:54 +0700

    update pertemuan 31

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,90 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Sidebar from "./assets/components/Sidebar";
+import axios from 'axios';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
-  const handleSidebar = () => {
-    setIsOpen(!isOpen);
+  // const handleSidebar = () => {
+  //   setIsOpen(!isOpen);
+  // };
+
+  // const [count, setCount] = useState(0);
+
+  // const handleAdd = paramNumber => {
+  //   const calculate = count + paramNumber;
+
+  //   setCount(calculate);
+  // }
+
+  const [product, setProduct] = useState([]);
+
+  const getData = () => {
+    axios
+      .get('https://api.escuelajs.co/api/v1/products?offset=0&limit=10')
+      .then(res => {
+        setProduct(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
   };
 
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState([]);
 
-  const handleAdd = paramNumber => {
-    const calculate = count + paramNumber;
+  const getUser = async () => {
+    try {
+      const res = await axios.get('https://api.escuelajs.co/api/v1/users');
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    setCount(calculate);
-  }
+  useEffect(() => {
+    getData();
+    getUser();
+  }, []);
 
   return (
-    <div>
-      <Sidebar isOpen={isOpen} handleSidebar={handleSidebar} handleAdd={handleAdd} count={count} />
+    <div className="grid grid-cols-2 gap-8">
+      {/* <Sidebar isOpen={isOpen} handleSidebar={handleSidebar} handleAdd={handleAdd} count={count} /> */}
+      {/* looping state product(title, price, description) */}
+      <div>
+        <h1 className="text-2xl font-bold">Product List</h1>
+        <ul className="w-1/2">
+          {product.map(product => (
+            <li key={product.id}>
+              <h2 className="text-xl font-medium">{product.title}</h2>
+              <p>Price: ${product.price}</p>
+              <p>{product.description}</p>
+              {product.images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  alt={product.title}
+                  className="flex gap-4 object-cover"
+                />
+              ))}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h1 className="text-2xl font-bold">User</h1>
+        <ul className="aspect-square size-40">
+          {user.map(user => (
+            <li key={user.id}>
+              <img
+                src={user.avatar}
+                alt={user.name}
+                className="obejct-cover rounded-full"
+              />
+              <h2 className="text-xl font-medium">{user.name}</h2>
+              <p>{user.email}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
